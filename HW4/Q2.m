592606bfab01eb71c9dbea950d842d53dcad0561
Md. Ismail Hossain <statistician71@gmail.com>
2022-10-03 15:48:17 -0600

    Add files via upload
    Updated file

--- HW4/Q2.m
+++ HW4/Q2.m
@@ -1,10 +1,11 @@
+cd C:\Users\TA-3\Desktop\OR\HW4
 
 %Phase-1
 
 % Given Information
 A = [1 2 1 -1 1 0;-3 1 1 3 0 1]
 b = [7;9]
-c = [1;-3;-6;1;0;0]
+c = [0;0;0;0;1;1]
 u = [1;5;8;1; Inf; Inf]
 
 %Initial basis x5 and x6
@@ -13,8 +14,8 @@ nonbasis0 = [1 2 3 4]
 nonbasisu = []
 basisupdateu
 
-%x3 enters basis, by Dantzig's rule
-enteringvar = 3
+%x2 enters basis, by Dantzig's rule
+enteringvar = 2
 
 a = A(:,enteringvar)
 d = solveBxb(L, U, p, a)
@@ -24,8 +25,8 @@ eps2 = 1.0e-4 ; eps3 = 1.0e-4
 [tlimit,leavingvar,leavingbound] = ratiotestu(basis,xb,ub,d,u(enteringvar),eps2,eps3)
 
 
-basis = [3 6]
-nonbasis0= [1 2 4 5]
+basis = [2 6]
+nonbasis0= [1 3 4 5]
 nonbasisu = []
 
 basisupdateu
@@ -36,16 +37,81 @@ enteringvar = 4
 a = A(:,enteringvar)
 d = solveBxb(L, U, p, a)
 
-eps2 = 1.0e-4 ; eps3 = 1.0e-4
 
 [tlimit,leavingvar,leavingbound] = ratiotestu(basis,xb,ub,d,u(enteringvar),eps2,eps3)
 
-basis = [3 4]
-nonbasis0= [1 2 5 6]
-nonbasisu = []
+% as non of the basis is becomem 0 if we increase x4 to it's highest limit.
+% x4 enters the nonbasisu
+
+basis = [2 6]
+nonbasis0= [1 3 5]
+nonbasisu = [4]
 
 basisupdateu
 
-%LP is optimal and phase 1 complete. The starting basis for Phase 2 is x3
-%and x4 (But I have confusion on that because the solution for x3 = -7 and 
-% x4 = 9 1st iteration phase 2, so need to think from here to solve this problem)
+
+%x3 enters basis, by Dantzig's rule
+enteringvar = 3
+
+a = A(:,enteringvar)
+d = solveBxb(L, U, p, a)
+
+
+[tlimit,leavingvar,leavingbound] = ratiotestu(basis,xb,ub,d,u(enteringvar),eps2,eps3)
+
+basis = [2 3]
+nonbasis0= [1 6 5]
+nonbasisu = [4]
+
+basisupdateu
+
+% The LP is optimal and z = 0. So, we can use x2 and x3 as our initial
+% basis for phase 2
+
+
+% Given Information
+A = [1 2 1 -1;-3 1 1 3]
+b = [7;9]
+c = [1;-3;-6;1]
+u = [1;5;8;1]
+
+
+%Initial basis x2 and x3
+basis = [2 3]
+nonbasis0 = [1]
+nonbasisu = [4]
+basisupdateu
+
+%x1 enters basis, by Dantzig's rule
+enteringvar = 1
+
+a = A(:,enteringvar)
+d = solveBxb(L, U, p, a)
+
+
+[tlimit,leavingvar,leavingbound] = ratiotestu(basis,xb,ub,d,u(enteringvar),eps2,eps3)
+
+basis = [1 3]
+nonbasis0 = [2]
+nonbasisu = [4]
+basisupdateu
+
+%The sign of x4 is positive, so if we reduce x4 then objective function
+%should decrease. 
+
+enteringvar = 4
+
+a = A(:,enteringvar)
+d = solveBxb(L, U, p, a)
+
+
+[tlimit,leavingvar,leavingbound] = ratiotestu(basis,xb,ub,-d,u(enteringvar),eps2,eps3)
+
+
+basis = [4 3]
+nonbasis0 = [1 2]
+nonbasisu = []
+basisupdateu
+
+
+% So, we found an optimal solution for the LP.
